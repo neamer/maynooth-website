f6811fd9fe53ca91d41bd3a076aed0b5da2265fa
Amer Nezirić <ne_amer@hotmail.com>
2020-10-01 15:44:23 +0200

    Add the Product, Color and ProductPicture models

--- main/products/models.py
+++ main/products/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+def get_upload_location(instance, filename):
+    return f'ProductPictures/{instance.product.name}/'
+
+
+class Color(models.Model):
+    name = models.CharField(max_length=20)
+    hexa = models.CharField(max_length=7)
+
+
+class Product(models.Model):
+    LIVINGROOM = 'L'
+    BEDROOM = 'B'
+    KITCHEN_DINING = 'K'
+
+    CATEGORY = [
+        (LIVINGROOM, 'Living room'),
+        (BEDROOM, 'Bedroom'),
+        (KITCHEN_DINING, 'Kitchen & dining'),
+    ]
+
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    short_desc = models.CharField(max_length=150)
+    detail_desc = models.TextField()
+    colors = models.ManyToManyField(Color, on_delete=models.SET_NULL)
+    category = models.CharField(
+        max_length=2, choices=CATEGORY, default=LIVINGROOM)
+    in_stock = models.BooleanField(default=True)
+    date_added = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+
+class ProductPicture(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL)
+    picture = models.ImageField(upload_to=get_upload_location, required=True)
