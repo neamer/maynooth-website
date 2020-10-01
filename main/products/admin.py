5690979b79c3d33edd3483c3ff2de99e29372db4
Amer Nezirić <ne_amer@hotmail.com>
2020-10-01 15:48:20 +0200

    Register the products models to the admin site

--- main/products/admin.py
+++ main/products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Color, Product, ProductPicture
 
-# Register your models here.
+admin.site.register(Color)
+admin.site.register(Product)
+admin.site.register(ProductPicture)
