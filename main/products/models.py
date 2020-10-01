2652deb819821b0162d4cb8acce2eee319d7b911
Amer Nezirić <ne_amer@hotmail.com>
2020-10-01 15:54:41 +0200

    make initial and product migrations

--- main/products/models.py
+++ main/products/models.py
@@ -37,6 +37,6 @@ class Product(models.Model):
 
 
 class ProductPicture(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
     picture = models.ImageField(
         upload_to=get_upload_location, default="no-picture.png")
