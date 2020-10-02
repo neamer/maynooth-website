4bca3f3c31f2687dd778933b95a40df801639aea
Amer Nezirić <ne_amer@hotmail.com>
2020-10-02 21:24:08 +0200

    Working on the header

--- main/frontend/src/components/layout/Header.js
+++ main/frontend/src/components/layout/Header.js
@@ -1,9 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./Header.css";
 import Logo from "./Logo-Maynooth-Option1.svg";
 
 function Header() {
+  const [dropDown, setDropDown] = useState(false);
+
+  const handleDropDown = (status) => setDropDown(status);
+
   return (
     <div className="navbar">
       <div className="navbar-container">
@@ -17,8 +21,26 @@ function Header() {
           <li className="nav-item">
             <div className="nav-icon"></div>
           </li>
-          <li className="nav-item">
-            <div id="nav-dropdown-btn">SHOP BY ROOM</div>
+          <li className="nav-item nav-dropdown">
+            <div className="nav-dropdown-square"></div>
+            <div
+              id="nav-dropdown-btn"
+              onMouseLeave={() => handleDropDown(false)}
+              onMouseOver={() => handleDropDown(true)}
+            >
+              SHOP BY ROOM
+            </div>
+            <ul
+              className={
+                dropDown
+                  ? "nav-dropdown-container"
+                  : "nav-dropdown-container display-none"
+              }
+            >
+              <li className="nav-dropdown-item">LIVING ROOM</li>
+              <li className="nav-dropdown-item">BEDROOM</li>
+              <li className="nav-dropdown-item">KITCHEN & DINING</li>
+            </ul>
           </li>
         </ul>
       </div>
