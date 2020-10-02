e063835c11901037ff029efc9fe8e25b19684e65
neamer <ne_amer@hotmail.com>
2020-10-03 01:49:27 +0200

    nav dropdown and responsive button

--- main/frontend/src/components/layout/Header.js
+++ main/frontend/src/components/layout/Header.js
@@ -1,12 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./Header.css";
 import Logo from "./Logo-Maynooth-Option1.svg";
+import DropDownArrow from "./dropdown.svg";
+
+// CHANGE TO HAMBURGER MENU ICON
+
+function ToggleMenuIcon(props) {
+  return <div className={props.className}></div>;
+}
 
 function Header() {
   const [dropDown, setDropDown] = useState(false);
+  const [showButton, setShowButton] = useState(true);
+  const [showMenu, setShowMenu] = useState(false);
 
+  const handleButton = (status) => setShowButton(status);
   const handleDropDown = (status) => setDropDown(status);
+
+  useEffect(() => {
+    if (window.innerWidth >= 750) {
+      setShowMenu(true);
+      setShowButton(false);
+    }
+  }, []);
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth >= 750) {
+      setShowButton(false);
+      setShowMenu(true);
+    } else {
+      setShowButton(true);
+      setShowMenu(false);
+    }
+  });
 
   return (
     <div className="navbar">
@@ -14,21 +41,36 @@ function Header() {
         <a href="#">
           <Logo className="nav-logo" />
         </a>
-        <ul className="nav-menu">
+
+        <ToggleMenuIcon
+          className={showButton ? "nav-icon" : "nav-icon display-none"}
+        />
+
+        <ul className={showMenu ? "nav-menu" : "display-none"}>
           <li className="nav-item">
             <div className="nav-icon"></div>
           </li>
           <li className="nav-item">
             <div className="nav-icon"></div>
           </li>
-          <li className="nav-item nav-dropdown">
+          <li
+            className="nav-item nav-dropdown"
+            onMouseLeave={() => handleDropDown(false)}
+          >
             <div
               id="nav-dropdown-btn"
-              onMouseLeave={() => handleDropDown(false)}
               onMouseOver={() => handleDropDown(true)}
+              onClick={() => handleDropDown(!dropDown)}
             >
               SHOP BY ROOM
             </div>
+            <DropDownArrow
+              className={
+                dropDown
+                  ? "dropdown-arrow dropdown-arrow-active"
+                  : "dropdown-arrow"
+              }
+            />
             <ul
               className={
                 dropDown
