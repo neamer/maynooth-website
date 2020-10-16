8138350d475782555ccc41426ff81847c4c62a9f
neamer <ne_amer@hotmail.com>
2020-10-17 00:50:20 +0200

    Start react router integration

--- main/frontend/src/components/App.js
+++ main/frontend/src/components/App.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { render } from "react-dom";
 
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
+
 import "./App.css";
 
-import LandingPage from './layout/landingPage';
+import LandingPage from "./layout/landingPage";
+import SearchResults from "./layout/searchResults";
 
 function App() {
   return (
     <>
-      <LandingPage />
+      <Router>
+        <Switch>
+          <Route path="/search" exact component={SearchResults} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </Router>
     </>
   );
 }
